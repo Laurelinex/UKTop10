5dc0835c05e369d6c0f134df7afe511453d7bdb4
Laureline Vaucoy <laureline.vaucoy@gmail.com>
2021-04-05 15:07:30 +0100

    homepage styled

--- src/components/Home.js
+++ src/components/Home.js
@@ -3,7 +3,7 @@ const Home = () => {
     return(
         <>
         <h1>Top Stuff on iTunes</h1>
-        <img src="https://upload.wikimedia.org/wikipedia/commons/2/2a/ITunes_12.2_logo.png" alt="iTunes" />
+        <img src="https://www.pngkit.com/png/full/57-576353_free-png-happy-men-png-images-transparent-transparent.png" alt="happy guy" />
         </>
     )
 }
--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -3,7 +3,7 @@ import {Link} from 'react-router-dom';
 const NavBar = () => {
     return(
         <div className="nav-bar">
-            <h1>Top Stuff</h1>
+            <img src="https://upload.wikimedia.org/wikipedia/commons/2/2a/ITunes_12.2_logo.png" alt="iTunes" height="50"/>
             <ul>
                 <li><Link to="/">Home</Link></li>
                 <li><Link to="/songs">Top Songs</Link></li>
