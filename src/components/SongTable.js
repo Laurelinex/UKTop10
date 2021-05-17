422a82a3c84dc83eeec675fe76245e95f36e5ffd
Laureline Vaucoy <laureline.vaucoy@gmail.com>
2021-05-17 14:09:30 +0100

    table skeleton setup

--- src/components/SongTable.js
+++ src/components/SongTable.js
@@ -2,7 +2,19 @@ const SongTable = ({songs}) => {
 
     return (
         <table className="song-table">
-            
+            <tbody>
+                <tr>
+                    <td rowSpan="3">number r1</td>
+                    <td rowSpan="3">image r1</td>
+                    <td>TITLE r1</td>
+                </tr>
+                <tr>
+                    <td>Artists</td>
+                </tr>
+                <tr>
+                    <td>Player</td>
+                </tr>
+            </tbody>
         </table>
     );
 
