8342fd63481b78fd0dc117b9fd14bbe971a9d324
Laureline Vaucoy <laureline.vaucoy@gmail.com>
2021-05-20 12:24:10 +0100

    restyled navbar for mobiles

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,15 +1,17 @@
 import {Link} from 'react-router-dom';
 import logo from '../images/UKTOP10.png'
+import logoMobile from "../images/ukchartslogo.png"
 
 const NavBar = () => {
     return(
         <div className="nav-bar">
             <img className="logo" src={logo} alt="logo"/>
+            <img className="logo-mobile" src={logoMobile} alt="logo"/>
             <ul>
-                <li><Link to="/">All</Link></li>
-                <li><Link to="/rock">Rock</Link></li>
-                <li><Link to="/dance">Dance</Link></li>
-                <li><Link to="/country">Country</Link></li>
+                <li><Link className="nav-link" to="/">All</Link></li>
+                <li><Link className="nav-link" to="/rock">Rock</Link></li>
+                <li><Link className="nav-link" to="/dance">Dance</Link></li>
+                <li><Link className="nav-link" to="/country">Country</Link></li>
             </ul>
         </div>
     )
